81569c49187050afa0bff248f40b3b06cf6d0245
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-08-28 20:12:40 +0200

    Add submitHandler method to update goals array

--- components/Form.js
+++ components/Form.js
@@ -3,17 +3,22 @@ import { View, Text, TextInput, Button } from "react-native";
 
 const Form = () => {
 
-  const [textInput, changeText] = useState('');
+  const [newGoal, changeText] = useState('');
+  const [goals, setGoals] = useState([]);
 
   const changeTextHandler = (inputValue) => {
     changeText(inputValue)
-  }
+  };
+
+  const submitHandler = () => {
+    setGoals(currentGoals => [...currentGoals, newGoal]);
+  };
 
   return (
     <View>
       <TextInput
         placeholder="What's your next goal?"
-        value={textInput}
+        value={newGoal}
         onChangeText={changeTextHandler}
       />
       <Button title="Add new goal" onPress={submitHandler} />
