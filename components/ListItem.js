d4ad62b1c40b0cb1b0a86e51d0ffad4cff11726e
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-08-29 22:48:10 +0200

    Create ListItem component

---
+++ components/ListItem.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const ListItem = () => {
+
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  )
+  
+}
+
+export default ListItem;
