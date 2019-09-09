322a4ba1b7a6a8e66960b3076cefd9faee22d894
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-09-09 22:40:51 +0200

    Add basic styling to list item

--- components/ListItem.js
+++ components/ListItem.js
@@ -1,15 +1,23 @@
 import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 
 const ListItem = props => {
 
   return (
     <TouchableOpacity onPress={() => alert('Clicked!')}>
-      <View>
+      <View style={styles.list}> 
         <Text>{props.title}</Text>
       </View>
     </TouchableOpacity>
   );
 };
+
+const styles = StyleSheet.create({
+  list: {
+    marginVertical: 5,
+    padding: 16,
+    backgroundColor: '#f9f9f9'
+  }
+});
 
 export default ListItem;
