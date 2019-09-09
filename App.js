59c8a0a59322b6fd230f3f5e54af0f196d10facc
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-09-09 23:35:29 +0200

    Add plus icon

--- App.js
+++ App.js
@@ -1,55 +1,55 @@
-import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, View, TouchableOpacity } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-import Form from './components/Form';
-import List from './components/List';
+import Form from "./components/Form";
+import List from "./components/List";
 
 const App = () => {
-
-  const [newGoal, changeText] = useState('');
+  const [newGoal, changeText] = useState("");
   const [goals, setGoals] = useState([]);
 
-  const changeTextHandler = (inputValue) => {
-    changeText(inputValue)
+  const changeTextHandler = inputValue => {
+    changeText(inputValue);
   };
 
   const submitHandler = () => {
     setGoals(currentGoals => [
-      ...currentGoals, 
+      ...currentGoals,
       {
         title: newGoal,
         key: Math.random().toString()
       }
-    ]);    
+    ]);
   };
 
   const removeGoalHandler = goalId => {
     setGoals(currentGoals => {
       return currentGoals.filter(goal => goal.key !== goalId);
-    })
-  }
+    });
+  };
 
   return (
     <View style={styles.container}>
       <Form
-      onChangeText={changeTextHandler}
-      onSubmit={submitHandler}
-      newGoal={newGoal}
+        onChangeText={changeTextHandler}
+        onSubmit={submitHandler}
+        newGoal={newGoal}
       />
-      <List
-        goals={goals}
-        removeGoalHandler={removeGoalHandler}
-      />
+      <List goals={goals} removeGoalHandler={removeGoalHandler} />
+      <TouchableOpacity>
+        <MaterialCommunityIcons name="plus" size={62} />
+      </TouchableOpacity>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#fff',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "center",
     padding: 30
-  },
+  }
 });
 
 export default App;
