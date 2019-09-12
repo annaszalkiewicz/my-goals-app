4e898fd1f9bef39eab3ba274ce69b48a14bdc677
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-09-12 23:14:09 +0200

    Add styling to input

--- components/Form.js
+++ components/Form.js
@@ -3,16 +3,17 @@ import { View, TextInput, Button, Modal, StyleSheet } from "react-native";
 
 const Form = props => {
   return (
-    <Modal
-      visible={props.visible}
-      animationType="slide"
-    >
+    <Modal visible={props.visible} animationType="slide">
       <View style={styles.container}>
-        <TextInput
-          placeholder="What's your next goal?"
-          value={props.newGoal}
-          onChangeText={props.onChangeText}
-        />
+        <View style={styles.inputContainer}>
+          <TextInput
+            placeholder="What's your next goal?"
+            placeholderTextColor="#000"
+            value={props.newGoal}
+            onChangeText={props.onChangeText}
+            style={styles.input}
+          />
+        </View>
         <Button title="Add new goal" onPress={props.onSubmit} />
       </View>
     </Modal>
@@ -25,6 +26,15 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     padding: 30
+  },
+  inputContainer: {
+    width: '80%',
+    maxWidth: 500,
+    borderBottomWidth: 2,
+    borderBottomColor: "#000"
+  },
+  input: {
+    paddingVertical: 10
   }
 });
 
